6105fa3792214be2ac8d08767731552ab9eaa3fe
Mark Fisher <mark.j.fisher@gmail.com>
2024-04-06 20:30:52 +0100

    Cleaned example code up

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
 
-int foo();
-int bar();
 
 int main() {
-	int i;
-	printf("hello, ");
-	i = foo();
-	printf("i = %d\n", i);
-	i = bar();
-	printf("i = %d\n", i);
 
-	cgetc();
 	return 0;
 }
